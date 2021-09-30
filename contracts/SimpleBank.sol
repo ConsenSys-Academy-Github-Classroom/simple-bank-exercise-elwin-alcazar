0b8a9c22d13cd8bfa77c90ec96a58a66c1f60953
Elwin-Alcazar <janelwin_alcazar@yahoo.com>
2021-09-30 21:22:40 +1300

    completes function withdraw

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -99,10 +99,15 @@ contract SimpleBank {
       // return the user's balance.
 
       // 1. Use a require expression to guard/ensure sender has enough funds
+      require(balances[msg.sender] >= withdrawAmount);
 
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
+      balances[msg.sender] -= withdrawAmount;
+      msg.sender.transfer(withdrawAmount);
 
       // 3. Emit the appropriate event for this message
+      emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+      return balances[msg.sender];
     }
 }
